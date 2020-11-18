3bba06c3b132da3f771cb565b1f18041225661a7
Martinique <me@martidolce.com>
2020-11-18 04:17:11 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,35 @@
-# Your code goes here!
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+#
+#
+
+class Anagrams
+  attr_accessor :answer
+
+  def initialize(answer)
+	  @answer = answer
+
+  end
+  # Ask for user to enter word
+
+  
+  def find_anagrams(answer)
+	  answer.each do |anagram|
+		  answer = gets.chomp
+		  # should detect no matches
+		  if anagram.match == false
+			  puts "Detect no matches"
+		  end
+	  end
+
+	  
+  # should detect a simple anagram
+
+  # should detect an anagram
+
+  # should detect multiple anagrams
+  end
+end
